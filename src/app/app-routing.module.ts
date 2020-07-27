a774385a48ed338b6f90ad77d9b23c9007274e23
Diego Torres <diegotc55@gmail.com>
2020-07-27 10:00:20 -0500

    Add routerLink imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {  ProductsComponent} from "./components/products/products.component";
+import { AboutComponent } from "./components/about/about.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'productos', component: ProductsComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'productos' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
